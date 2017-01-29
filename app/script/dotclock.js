9c13a95f0f7a30821d9165815141fd8d1a404689
wertrain <wertrain@gmail.com>
2017-01-29 18:30:48 +0900

    リファクタ

--- app/script/dotclock.js
+++ app/script/dotclock.js
@@ -1,59 +1,27 @@
 $(function(){
-
-  // Cache some selectors
-
-  var clock = $('#clock');
-
-  // Map digits to their names (this will be an array)
-  var digit_to_name = 'zero one two three four five six seven eight nine'.split(' ');
-
-  // This object will hold the digit elements
-  var digits = {};
-
-  // Positions for the hours, minutes, and seconds
-  var positions = [
+  let clock = $('#clock');
+  let digit_to_name = 'zero one two three four five six seven eight nine'.split(' ');
+  let digits = {};
+  let positions = [
     'h1', 'h2', ':', 'm1', 'm2', ':', 's1', 's2'
   ];
-
-  // Generate the digits with the needed markup,
-  // and add them to the clock
-
-  var digit_holder = clock.find('.digits');
-
-  $.each(positions, function(){
-
-    if(this == ':'){
+  let digit_holder = clock.find('.digits');
+  $.each(positions, function() {
+    if(this == ':') {
       digit_holder.append('<div class="dots">');
-    }
-    else{
-
-      var pos = $('<div>');
-
-      for(var i=1; i<8; i++){
+    } else {
+      let pos = $('<div>');
+      for(let i = 1; i < 8; ++i) {
         pos.append('<span class="d' + i + '">');
         pos.append('<span class="d' + i + 'b">');
       }
-
-      // Set the digits as key:value pairs in the digits object
       digits[this] = pos;
-
-      // Add the digit elements to the page
       digit_holder.append(pos);
     }
-
   });
 
-  // Run a timer every second and update the clock
-
-  (function update_time(){
-
-    // Use moment.js to output the current time as a string
-    // hh is for the hours in 12-hour format,
-    // mm - minutes, ss-seconds (all with leading zeroes),
-    // d is for day of week and A is for AM/PM
-
-    var now = moment().format("hhmmssdA");
-
+  (function update_time() {
+    let now = moment().format("hhmmssdA");
     digits.h1.attr('class', digit_to_name[now[0]]);
     digits.h2.attr('class', digit_to_name[now[1]]);
     digits.m1.attr('class', digit_to_name[now[2]]);
@@ -61,11 +29,10 @@ $(function(){
     digits.s1.attr('class', digit_to_name[now[4]]);
     digits.s2.attr('class', digit_to_name[now[5]]);
 
-    var dow = now[6];
+    let dow = now[6];
     dow--;
-    
+
     // Schedule this function to be run again in 1 sec
     setTimeout(update_time, 1000);
-
   })();
 });
